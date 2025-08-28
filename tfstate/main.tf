6adf0591899b91aed6629e5cc119d305b8ca50b5
Andrey Kaipov <9457739+andreykaipov@users.noreply.github.com>
2025-08-27 23:12:12 -0400

    tfstate: migrate to v5 cf

--- tfstate/main.tf
+++ tfstate/main.tf
@@ -8,30 +8,37 @@ resource "cloudflare_workers_kv_namespace" "tfstate" {
 }
 
 resource "cloudflare_workers_script" "tfstate" {
-  account_id = local.cf_account_id
-  name       = "tfstate-handler"
-  content    = file("index.js")
-  module     = true
-  kv_namespace_binding {
-    name         = cloudflare_workers_kv_namespace.tfstate.title
-    namespace_id = cloudflare_workers_kv_namespace.tfstate.id
-  }
-  secret_text_binding {
-    name = "username"
-    text = local.secrets.setup["tf_backend_username"]
-  }
-  secret_text_binding {
-    name = "password"
-    text = local.secrets.setup["tf_backend_password"]
-  }
+  account_id  = local.cf_account_id
+  script_name = "tfstate-handler"
+  content     = file("index.js")
+  main_module = "worker.js"
+  # we want the bindings to be sorted by name to guarantee idempotency, and
+  # mapping from a map to a list implicitly sorts the objects by the name key
+  bindings = [
+    for k, v in {
+      username = {
+        type = "secret_text"
+        text = local.secrets.setup["tf_backend_username"]
+      }
+      password = {
+        type = "secret_text"
+        text = local.secrets.setup["tf_backend_password"]
+      }
+      (cloudflare_workers_kv_namespace.tfstate.title) = {
+        type         = "kv_namespace"
+        namespace_id = cloudflare_workers_kv_namespace.tfstate.id
+      }
+  } : merge(v, { name = k })]
 }
 
 data "cloudflare_zone" "kaipov" {
-  name = "kaipov.com"
+  filter = {
+    name = "kaipov.com"
+  }
 }
 
 resource "cloudflare_workers_route" "terraform_route" {
-  zone_id     = data.cloudflare_zone.kaipov.zone_id
-  pattern     = "tf.${data.cloudflare_zone.kaipov.name}/*"
-  script_name = cloudflare_workers_script.tfstate.name
+  zone_id = data.cloudflare_zone.kaipov.zone_id
+  pattern = "tf.${data.cloudflare_zone.kaipov.name}/*"
+  script  = cloudflare_workers_script.tfstate.script_name
 }
